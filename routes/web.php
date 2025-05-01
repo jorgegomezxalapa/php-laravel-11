fc65ae5aa214df29d1509f7c3b52e789cdca574e
Jorge Ivan <jrgz305@gmail.com>
2025-04-30 21:42:59 -0600

    Se agrega rutas y funciones del controlador Obras
    Se agregan los métodos y las rutas para ejecutar las funciones create, read, delete y update del modelo Obra.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("obras.index");
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/obras', [App\Http\Controllers\ObraController::class, 'index'])->name('obras.index');
+Route::get('/obras/create/{id?}', [App\Http\Controllers\ObraController::class, 'create'])->name('obras.create');
+Route::post('/obras/store', [App\Http\Controllers\ObraController::class, 'store'])->name('obras.store');
+Route::get('/obras/show/{id}', [App\Http\Controllers\ObraController::class, 'show'])->name('obras.show');
+Route::post('/obras/delete', [App\Http\Controllers\ObraController::class, 'delete'])->name('obras.delete');
+
