f75fe4ae368126e7a452045eb3a5b73b58778788
Jorge Ivan <jrgz305@gmail.com>
2025-05-02 12:20:24 -0600

    Agregando relación de incidentes en  obras
    Se agrega el update de relación de incidentes en el controlador, modelo y la vista del crud Obras

--- app/Http/Controllers/ObraController.php
+++ app/Http/Controllers/ObraController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Incidente;
 use App\Models\Obra;
 use Illuminate\Http\Request;
 
@@ -14,29 +15,57 @@ class ObraController extends Controller
 
     public function create($id = null){
         $obra = Obra::find($id);
-        return view("obras.form", compact("obra"));
+        $incidentes = Incidente::all();
+        $incidentesSeleccionados = $obra ? ($obra->incidentes ? $obra->incidentes->pluck('id')->toArray() : [])  : [];
+        return view("obras.form", compact("obra", "incidentes", "incidentesSeleccionados"));
     }
 
     public function store(Request $request){
-            $obraData = [
-                'numero' => $request->numero_obra,
-                'nombre' => $request->nombre_obra,
-                'clave' => $request->clave_obra,
-                'objeto' => $request->objeto_obra,
-                'direccion' => $request->direccion,
-                'latitud' => $request->latitud,
-                'longitud' => $request->longitud,
-                'galeria_imagenes' => json_encode([]),
-            ];
-            if($request->obra_id){
-                $obra = Obra::find($request->obra_id);
+        $obraData = [
+            'numero' => $request->numero_obra,
+            'nombre' => $request->nombre_obra,
+            'clave' => $request->clave_obra,
+            'objeto' => $request->objeto_obra,
+            'direccion' => $request->direccion,
+            'latitud' => $request->latitud,
+            'longitud' => $request->longitud
+        ];
+    
+        if ($request->obra_id) {
+            $obra = Obra::find($request->obra_id);
+            if ($obra) {
                 $obra->update($obraData);
                 $accion = "actualizado";
-            }else{
-                Obra::create($obraData);
-                $accion = "registrado";
-            }
         
+                $obra->incidentes()->sync([]);
+        
+                if ($request->has('incidentes')) {
+                    $obra->incidentes()->sync($request->incidentes);
+                }
+            } else {
+                return redirect()->route("obras.index")->with("error", "La obra no se encontró.");
+            }
+        } else {
+            $images = $this->pexelsService->getConstructionImages(5);
+            $formattedImages = array_map(function ($image) {
+                return [
+                    'url' => $image['src']['original'],
+                    'photographer' => $image['photographer'],
+                    'photographer_url' => $image['photographer_url']
+                ];
+            }, $images);
+        
+            $obraData['galeria_imagenes'] = json_encode($formattedImages);
+        
+            $obra = Obra::create($obraData);
+            $accion = "registrado";
+        
+            if ($request->has('incidentes')) {
+                $obra->incidentes()->sync($request->incidentes);
+            }
+        }
+        
+
         return redirect()->route("obras.index")->with("success", "La obra se ha $accion con éxito");
     }
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -55,3 +55,15 @@ const vectorLayer = new VectorLayer({
 
 map.addLayer(vectorLayer);
 
+const eliminar_incidente = document.getElementsByClassName("btn-eliminar-incidente");
+
+for (let index = 0; index < eliminar_incidente.length; index++) {
+    const element = eliminar_incidente[index];
+    element.addEventListener("click", function(){
+        document.getElementById("eliminar_incidente_id").value = element.dataset.incidente_id;
+    })
+    
+}
+
+
+
