ec0fc9d069e6e786681356a3b294cd2c50164123
Jorge Ivan <jrgz305@gmail.com>
2025-05-01 20:32:10 -0600

    Agregando variables pexels
    Se agregan las variables globales para el uso de la api de pexels

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'pexels' => [
+        'api_key' => env('PEXELS_API_KEY'),
+        'count' => env('PEXELS_COUNT'),
+    ],
 
 ];
